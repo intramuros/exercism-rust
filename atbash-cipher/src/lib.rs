594407453e506076221fc6d0cd7394c3c6b972ae
sesegma <sesegma@gmail.com>
2018-10-09 01:08:21 +0200

    atbash, 2nd iteration after comments

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,32 +1,31 @@
 use std::char;
+use std::iter;
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    let encoded = plain
+    plain
         .chars()
-        .filter(|s| s.is_ascii())
         .filter_map(|s| match s {
             '0'...'9' => Some(s),
             'a'...'z' | 'A'...'Z' => s
                 .to_digit(36)
                 .and_then(|num| char::from_digit(35 - num + 10, 36)),
             _ => None,
-        }).collect::<Vec<_>>();
-    let mut final_string = String::new();
-    for (i, &x) in encoded.iter().enumerate() {
-        if i % 5 == 0 && i != 0 {
-            final_string.push(' ');
-        }
-        final_string.push(x);
-    }
-    final_string
+        }).enumerate()
+        .flat_map(|(i, x)| {
+            if i % 5 == 0 && i != 0 {
+                iter::once(' ').chain(iter::once(x))
+            } else {
+                iter::once('*').chain(iter::once(x))
+            }
+        }).filter(|&s| s != '*')
+        .collect::<String>()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
     cipher
         .chars()
-        .filter(|s| s.is_ascii())
         .filter_map(|s| match s {
             '0'...'9' => Some(s),
             'a'...'z' | 'A'...'Z' => s
